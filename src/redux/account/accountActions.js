1d5e017e232849beb0540929dfdb507b2d95dc3b
ariclenespedro <pedroariclenes@gmail.com>
2024-04-01 17:59:40 +0100

    redux: implementação da requisição get data account

--- src/redux/account/accountActions.js
+++ src/redux/account/accountActions.js
@@ -8,6 +8,17 @@ export const getAllDataAccount = createAsyncThunk(
         try {
             const session = await getSession();
             console.log(session);
+            const token = session?.data.token;
+            const client_id = session?.user.client_id; 
+
+            const config = {
+                headers: {
+                  "authorization": token,
+                },
+            };
+            const res = await axios.get(`http://192.168.1.76:3000/api/client/${client_id}/account`,config);
+            return res.data;
+
         } catch (error) {
             
         }
